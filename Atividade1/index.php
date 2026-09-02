8bb59bd0e7dc9628d686ad424b3ee4f2bb3ba735
João Marcos <joao.m.soares12@aluno.senai.br>
2026-09-02 16:17:33 -0300

    index atividade 1 adicionado

---
+++ Atividade1/index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Análise de Saúde Financeira</title>
+
+    <style>
+
+    </style>
+</head>
+<body>
+    <header>
+        <h1>💰 Analisador de Saúde Financeira</h1>
+        <h4>Entenda seus hábitos e planeje seu futuro com segurança</h4>  
+    </header>
+
+    <nav>
+        <div class="Menu">
+            <div><a href="index.php">Início<a></div>
+            <div><a href="index.php">Cadastro<a></div>
+            <div><a href="index.php">Lista de Cadastro<a></div>
+            <div><a href="index.php">Minha Análise<a></div>
+        </div>
+    </nav>
+
+    <main>
+        <img src="./img/Happy.webp">
+        <h1><strong>Bem-vindo ao Analisador de Saúde Financeira</strong></h1>
+        <h3>Este sistema foi desenvolvido para ajudar você, estudante a entender melhor seus hábitos de consumo e avaliar sua saúde financeira de forma simples e objetiva.</h3>
+    </main>
+</body>
+</html>
